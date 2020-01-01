1c6e4d7539f7e452d5851c957df09a0a5dfd8870
Eric Chen <ericchen9025@gmail.com>
2019-12-31 19:38:06 -0500

    tweet file complete

--- tweet.py
+++ tweet.py
@@ -3,13 +3,20 @@ from configs import configs
 
 
 def main():
-    global driver
-    bot = TwitterBot(configs["username"], configs["password"])
-    driver = bot.driver
-    latest_link = bot.get_latest_article_link()
-    bot.save_article_url_to_file(latest_link)
-    bot.login_twitter()
-    bot.tweet(latest_link)
+    try:
+        global driver
+        bot = TwitterBot(configs["username"], configs["password"])
+        driver = bot.driver
+        latest_link = bot.get_latest_article_link()
+        if bot.is_new_article(latest_link):
+            bot.save_article_url_to_file(latest_link)
+            bot.login_twitter()
+            bot.tweet(latest_link)
+            print('\033[92m' + "Successful")
+        else:
+            print('\033[94m' + "No new articles")
+    except:
+        print('\033[91m' + "Something went wrong")
 
 
 if __name__ == "__main__":
